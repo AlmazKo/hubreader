eae97748e3653dc38442a7759c3202c42d95a7da
Almazko <a.s.suslov@gmail.com>
2014-03-08 22:15:36 +0400

    #1 ann uniq index

--- src/com/github/hubreader/data/DatabaseHelper.java
+++ src/com/github/hubreader/data/DatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "habreader";
-    private static final int DATABASE_VERSION = 5;
+    private static final int DATABASE_VERSION = 6;
 
     DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
